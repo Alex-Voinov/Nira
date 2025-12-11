d6ebc0d91f87e8738dfc7ca0153284b020768057
Egor Docenko <egordocenko359@gmail.com>
2025-12-11 21:22:17 +0300

    добавил функцию определения возроста

---
+++ api/utils/Defining_age.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+#вот
+def defining_age(Birthday: str)
+
+    birth_date = datatime.today(Birthday, "%d.%m.%Y")
+    
+    today = datetime.today()
+    
+    age = today.year - birth_date.year
+    
+    if (today.month, today.day) < (birth_date.month, birth_date.day)
+        age =- 1
+    
+    return age
+    
+    
